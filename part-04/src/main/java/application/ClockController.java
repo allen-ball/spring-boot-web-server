da04cbf42822ac11d410ad4f073892f88cb347ae
Allen D. Ball <ball@hcf.dev>
2019-12-26 17:08:46 -0800

    Set all Model attributes programmatically in get() method. Use Locale-specific Collator for String compares when sorting. Use "@SessionAttribute Optional<String>" to access session attributes.

--- part-04/src/main/java/application/ClockController.java
+++ part-04/src/main/java/application/ClockController.java
@@ -1,11 +1,13 @@
 package application;
 
+import java.text.Collator;
 import java.text.DateFormat;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Optional;
 import java.util.TimeZone;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -16,10 +18,10 @@ import lombok.ToString;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 @RequestMapping(value = { "/clock/" })
@@ -28,58 +30,66 @@ public class ClockController {
     private static final List<Locale> LOCALES =
         Stream.of(Locale.getAvailableLocales())
         .filter(t -> (! t.toString().equals("")))
-        .sorted(Comparator.comparing(Locale::toLanguageTag))
         .collect(Collectors.toList());
     private static final List<TimeZone> ZONES =
         Stream.of(TimeZone.getAvailableIDs())
         .map(t -> TimeZone.getTimeZone(t))
-        .sorted(Comparator
-                .comparingInt(TimeZone::getRawOffset)
-                .thenComparingInt(TimeZone::getDSTSavings)
-                .thenComparing(TimeZone::getID))
         .collect(Collectors.toList());
 
-    @ModelAttribute
-    public void addAttributesTo(Model model, Locale locale, HttpSession session) {
-        String languageTag = (String) session.getAttribute("languageTag");
-
-        if (languageTag != null) {
-            locale = Locale.forLanguageTag(languageTag);
+    @RequestMapping(method = { RequestMethod.GET }, value = { "time" })
+    public void get(Model model, Locale locale, TimeZone zone,
+                    @SessionAttribute Optional<String> languageTag,
+                    @SessionAttribute Optional<String> zoneID) {
+        if (languageTag.isPresent()) {
+            locale = Locale.forLanguageTag(languageTag.get());
         }
 
-        TimeZone zone = TimeZone.getDefault();
-        String zoneID = (String) session.getAttribute("zoneID");
-
-        if (zoneID != null) {
-            zone = TimeZone.getTimeZone(zoneID);
+        if (zoneID.isPresent()) {
+            zone = TimeZone.getTimeZone(zoneID.get());
         }
 
-        model.addAttribute("locales", LOCALES);
-        model.addAttribute("zones", ZONES);
         model.addAttribute("locale", locale);
         model.addAttribute("zone", zone);
-        model.addAttribute("date", DateFormat.getDateInstance(DateFormat.LONG, locale));
-        model.addAttribute("time", DateFormat.getTimeInstance(DateFormat.MEDIUM, locale));
-        model.addAttribute("timestamp", new Date());
+
+        DateFormat date = DateFormat.getDateInstance(DateFormat.LONG, locale);
+        DateFormat time = DateFormat.getTimeInstance(DateFormat.MEDIUM, locale);
+
+        model.addAttribute("date", date);
+        model.addAttribute("time", time);
 
         for (Object object : model.asMap().values()) {
             if (object instanceof DateFormat) {
                 ((DateFormat) object).setTimeZone(zone);
             }
         }
-    }
 
-    @RequestMapping(method = { RequestMethod.GET }, value = { "time" })
-    public String get(HttpServletRequest request) {
-        return request.getServletPath();
+        Collator collator = Collator.getInstance(locale);
+        List<Locale> locales =
+            LOCALES.stream()
+            .sorted(Comparator.comparing(Locale::toLanguageTag, collator))
+            .collect(Collectors.toList());
+        List<TimeZone> zones =
+            ZONES.stream()
+            .sorted(Comparator
+                    .comparingInt(TimeZone::getRawOffset)
+                    .thenComparingInt(TimeZone::getDSTSavings)
+                    .thenComparing(TimeZone::getID, collator))
+            .collect(Collectors.toList());
+
+        model.addAttribute("locales", locales);
+        model.addAttribute("zones", zones);
+
+        Date timestamp = new Date();
+
+        model.addAttribute("timestamp", timestamp);
     }
 
     @RequestMapping(method = { RequestMethod.POST }, value = { "time" })
     public String post(HttpServletRequest request, HttpSession session,
                        @RequestParam Map<String,String> form) {
-        form.entrySet()
-            .stream()
-            .forEach(t -> session.setAttribute(t.getKey(), t.getValue()));
+        for (Map.Entry<String,String> entry : form.entrySet()) {
+            session.setAttribute(entry.getKey(), entry.getValue());
+        }
 
         return "redirect:" + request.getServletPath();
     }
