9b2b982bdb95004c7714386becef33385647fdcd
Allen D. Ball <ball@hcf.dev>
2020-10-29 13:33:42 -0700

    Added asGrantedAuthorityList() method.

--- part-07/src/main/java/application/jpa/AuthoritiesSet.java
+++ part-07/src/main/java/application/jpa/AuthoritiesSet.java
@@ -1,18 +1,27 @@
 package application.jpa;
 
 import application.Authorities;
+import java.util.List;
 import java.util.TreeSet;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toCollection;
 
 @NoArgsConstructor
 public class AuthoritiesSet extends TreeSet<Authorities> {
+    public List<GrantedAuthority> asGrantedAuthorityList() {
+        String[] strings = stream().map(Authorities::name).toArray(String[]::new);
+
+        return AuthorityUtils.createAuthorityList(strings);
+    }
+
     @Converter(autoApply = true)
     @NoArgsConstructor
     public static class ConverterImpl implements AttributeConverter<AuthoritiesSet,String> {
