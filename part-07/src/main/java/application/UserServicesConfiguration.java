8dec36b02b87928f1b34a5cfe39a6486148c3d80
Allen D. Ball <ball@hcf.dev>
2020-10-31 00:54:57 -0700

    Map granted authorities to OAuth 2.0 and OIDC users.

--- part-07/src/main/java/application/UserServicesConfiguration.java
+++ part-07/src/main/java/application/UserServicesConfiguration.java
@@ -4,6 +4,7 @@ import application.jpa.Authority;
 import application.jpa.AuthorityRepository;
 import application.jpa.Credential;
 import application.jpa.CredentialRepository;
+import java.util.List;
 import java.util.Optional;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -11,6 +12,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -22,6 +24,7 @@ import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserServ
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -74,22 +77,31 @@ public class UserServicesConfiguration {
         }
     }
 
+    private static final List<GrantedAuthority> DEFAULT_AUTHORITIES =
+        AuthorityUtils.createAuthorityList(Authorities.USER.name());
+
     @NoArgsConstructor @ToString
     private class OAuth2UserServiceImpl extends DefaultOAuth2UserService {
         private final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
 
         @Override
         public OAuth2User loadUser(OAuth2UserRequest request) throws OAuth2AuthenticationException {
+            String attribute =
+                request.getClientRegistration().getProviderDetails()
+                .getUserInfoEndpoint().getUserNameAttributeName();
             OAuth2User user = delegate.loadUser(request);
-
-            /* log.info("{}", request.getAccessToken().getTokenValue()); */
 
             try {
                 Optional<Authority> authority = authorityRepository.findById(user.getName());
+
+                user =
+                    new DefaultOAuth2User(authority.map(t -> t.getGrants().asGrantedAuthorityList())
+                                          .orElse(DEFAULT_AUTHORITIES),
+                                          user.getAttributes(), attribute);
             } catch (OAuth2AuthenticationException exception) {
                 throw exception;
             } catch (Exception exception) {
-                log.info("{}", request, exception);
+                log.warn("{}", request, exception);
             }
 
             return user;
@@ -102,7 +114,23 @@ public class UserServicesConfiguration {
 
         @Override
         public OidcUser loadUser(OidcUserRequest request) throws OAuth2AuthenticationException {
-            OidcUser super.loadUser(request);
+            String attribute =
+                request.getClientRegistration().getProviderDetails()
+                .getUserInfoEndpoint().getUserNameAttributeName();
+            OidcUser user = super.loadUser(request);
+
+            try {
+                Optional<Authority> authority = authorityRepository.findById(user.getName());
+
+                user =
+                    new DefaultOidcUser(authority.map(t -> t.getGrants().asGrantedAuthorityList())
+                                        .orElse(DEFAULT_AUTHORITIES),
+                                        user.getIdToken(), user.getUserInfo(), attribute);
+            } catch (OAuth2AuthenticationException exception) {
+                throw exception;
+            } catch (Exception exception) {
+                log.warn("{}", request, exception);
+            }
 
             return user;
         }
